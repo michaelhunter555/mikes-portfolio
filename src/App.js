e3bd8fcb14a3f5201e4cae1d35bfade1a3cfc43f
idioticbrialliance <dumbfreshdope@gmail.com>
2022-12-19 09:27:30 +0700

    fixed window scroll transition

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './index.scss';
 
 import React from 'react';
 
-import { AnimatePresence } from 'framer-motion';
 import {
   Route,
   Routes,
   useLocation,
 } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
 import About from './routes/About';
 import Contact from './routes/Contact';
 import Home from './routes/Home';
@@ -18,14 +18,13 @@ function App() {
   const location = useLocation();
   return (
     <>
-      <AnimatePresence exitBeforeEnter>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </AnimatePresence>
+      <ScrollToTop />
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
